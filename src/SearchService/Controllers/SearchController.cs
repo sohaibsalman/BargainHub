91991af3187d7d7617f2b283e94d2e45ef05ddd0
sohaibsalman <sohaibsalman10@gmail.com>
2023-11-08 22:37:53 +0500

    Add search functionality

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
+using SearchService.Entities;
+using SearchService.RequestHelpers;
 
 namespace SearchService.Controllers;
 
@@ -7,8 +10,50 @@ namespace SearchService.Controllers;
 public class SearchController : ControllerBase
 {
     [HttpGet]
-    public async Task<IActionResult> SearchItems(string searchTerm)
+    public async Task<IActionResult> SearchItems([FromQuery] SearchParams searchParams)
     {
-        
+        PagedSearch<Item, Item> query = GetPagedQuery(searchParams);
+        var results = await query.ExecuteAsync();
+
+        PagedResult<List<Item>> pagedResults = new()
+        {
+            Results = results.Results.ToList(),
+            TotalCount = (int) results.TotalCount,
+            PageCount = results.PageCount,
+        };
+
+        return Ok(pagedResults);
+    }
+
+
+    private PagedSearch<Item, Item> GetPagedQuery(SearchParams searchParams)
+    {
+        PagedSearch<Item, Item> query = DB.PagedSearch<Item, Item>();
+
+        query = searchParams.OrderBy switch
+        {
+            "make" => query.Sort(x => x.Ascending(a => a.Model)),
+            "new" => query.Sort(x => x.Descending(a => a.CreatedAt)),
+            _ => query.Sort(x => x.Ascending(a => a.AuctionEnd))
+        };
+
+        query = searchParams.FilterBy switch
+        {
+            "finished" => query.Match(x => x.AuctionEnd < DateTime.UtcNow),
+            "endingSoon" => query.Match(x => x.AuctionEnd < DateTime.UtcNow.AddHours(6) && x.AuctionEnd > DateTime.UtcNow),
+            _ => query.Match(x => x.AuctionEnd > DateTime.UtcNow)
+        };
+
+        if (!string.IsNullOrEmpty(searchParams.Seller))
+            query.Match(x => x.Seller == searchParams.Seller);
+        if (!string.IsNullOrEmpty(searchParams.Winner))
+            query.Match(x => x.Winner == searchParams.Winner);
+        if (!string.IsNullOrEmpty(searchParams.SearchTerm))
+            query.Match(Search.Full, searchParams.SearchTerm).SortByTextScore();
+
+        query.PageNumber(searchParams.PageNumber);
+        query.PageSize(searchParams.PageSize);
+
+        return query;
     }
 }
---
+++ src/SearchService/RequestHelpers/PagedResult.cs
@@ -0,0 +1,8 @@
+﻿namespace SearchService.RequestHelpers;
+
+public class PagedResult<T>
+{
+    public T Results { get; set; }
+    public int PageCount { get; set; }
+    public int TotalCount { get; set; }
+}
