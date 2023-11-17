8fc3aca6cd5501190f9087739b1551e1f72f167b
sohaibsalman <sohaibsalman10@gmail.com>
2023-11-17 21:47:40 +0500

    Add bid placed and auction finished consumers

---
+++ src/SearchService/Consumers/AuctionFinishedConsumer.cs
@@ -0,0 +1,24 @@
+﻿using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+using SearchService.Entities;
+
+namespace SearchService.Consumers;
+
+public class AuctionFinishedConsumer : IConsumer<AuctionFinished>
+{
+    public async Task Consume(ConsumeContext<AuctionFinished> context)
+    {
+        Console.WriteLine("--> Consuming auction finished");
+        Item auctionItem = await DB.Find<Item>().OneAsync(context.Message.AuctionId);
+
+        if (context.Message.ItemSold)
+        {
+            auctionItem.Winner = context.Message.Winner;
+            auctionItem.SoldAmount = (int) context.Message.Amount;
+        }
+        auctionItem.Status = "Finished";
+
+        await auctionItem.SaveAsync();
+    }
+}
---
+++ src/SearchService/Consumers/BidPlacedConsumer.cs
@@ -0,0 +1,21 @@
+﻿using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+using SearchService.Entities;
+
+namespace SearchService.Consumers;
+
+public class BidPlacedConsumer : IConsumer<BidPlaced>
+{
+    public async Task Consume(ConsumeContext<BidPlaced> context)
+    {
+        Console.WriteLine("--> Consuming bid placed");
+        Item auctionItem = await DB.Find<Item>().OneAsync(context.Message.AuctionId);
+
+        if (context.Message.BidStatus.Contains("Accepted") && context.Message.Amount > auctionItem.CurrentHighBid)
+        {
+            auctionItem.CurrentHighBid = context.Message.Amount;
+            await auctionItem.SaveAsync();   
+        }
+    }
+}
