4c63b5b36374579dfb368c0eee60f18fb164a001
sohaibsalman <sohaibsalman10@gmail.com>
2023-11-20 21:58:02 +0500

    Add next-auth for authentication with identity server

--- frontend/web-app/app/nav/Navbar.tsx
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Search from './Search';
 import Logo from './Logo';
+import LoginButton from './LoginButton';
 
 
 export default function Navbar() {
@@ -9,7 +10,7 @@ export default function Navbar() {
     <header className='sticky z-50 flex justify-between bg-white py-2 px-5 items-center text-gray-800 shadow-md'>
       <Logo />
       <Search />
-      <div>Login</div>
+      <LoginButton />
     </header>
   );
 }
